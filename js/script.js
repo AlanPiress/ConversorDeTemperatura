eda23c1fcd5197ba2a6b550d9ac3bae2a99c0ee3
Alan Pires <alanpires511@gmail.com>
2021-09-25 19:18:12 -0300

    Adicionando a função e definindo as variáveis - JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 document.getElementById("converte").onclick = tempConvert;
 document.getElementById("reset").onclick = resetForm;
+
+function tempConvert(){
+    var fahrenheit = document.getElementById("fahrenheit").value;
+    var celsius = document.getElementById("celsius").value;
+}
