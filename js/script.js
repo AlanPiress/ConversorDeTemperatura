568547286fcee918f9ab9a71ac7bfafcc1d32858
Alan Pires <alanpires511@gmail.com>
2021-09-25 19:24:37 -0300

    Adicionando a função reset e JS finalizado - JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.getElementById("converte").onclick = tempConvert;
 document.getElementById("reset").onclick = resetForm;
 
+
 function tempConvert(){
     var fahrenheit = document.getElementById("fahrenheit").value;
     var celsius = document.getElementById("celsius").value;
@@ -16,5 +17,10 @@ function tempConvert(){
 
     document.getElementById("fahrenheit").value = parseFloat(fahrenheit).toFixed(1);
     document.getElementById("celsius").value = parseFloat(celsius).toFixed(1);
-
 }
+
+function resetForm(){
+    document.getElementById("fahrenheit").value= "";
+    document.getElementById("celsius").value= "";
+}
+
